44a14d1b3fd323bac41b99e51bfb4ab8a0f84f90
Fidriyanto Rizkillah <frizkillah98@gmail.com>
2023-06-25 19:59:49 +0700

    Change font and add custom typography

--- core/ui/src/main/kotlin/com/rezyfr/foodmarket/core/ui/theme/Theme.kt
+++ core/ui/src/main/kotlin/com/rezyfr/foodmarket/core/ui/theme/Theme.kt
@@ -3,15 +3,25 @@ package com.rezyfr.foodmarket.core.ui.theme
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 
 @Composable
 fun FoodMarketTheme(
     useDarkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
-    MaterialTheme(
-        typography = FMTypography,
-        colors = FMColor,
-        content = content
-    )
+    CompositionLocalProvider(
+        LocalCustomTypography provides foodMarketTypography
+    ) {
+        MaterialTheme(
+            colors = FMColor,
+            content = content
+        )
+    }
+}
+
+object FMTheme {
+    val typography: CustomTypography
+        @Composable
+        get() = LocalCustomTypography.current
 }
--- core/ui/src/main/kotlin/com/rezyfr/foodmarket/core/ui/theme/Type.kt
+++ core/ui/src/main/kotlin/com/rezyfr/foodmarket/core/ui/theme/Type.kt
@@ -1,46 +1,177 @@
 package com.rezyfr.foodmarket.core.ui.theme
 
-
 import androidx.compose.material.Typography
-import androidx.compose.ui.text.ExperimentalTextApi
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.googlefonts.Font
-import androidx.compose.ui.text.googlefonts.GoogleFont
+import androidx.compose.ui.unit.sp
 import com.rezyfr.foodmarket.core.ui.R
 
-@ExperimentalTextApi
-internal fun createSingleGoogleFontFamily(
-    name: String,
-    provider: GoogleFont.Provider = GmsFontProvider,
-    weights: List<FontWeight>,
-): FontFamily = FontFamily(
-    weights.map { weight ->
-        Font(
-            googleFont = GoogleFont(name),
-            fontProvider = provider,
-            weight = weight,
-        )
-    },
+val fonts = FontFamily(
+    Font(R.font.eudoxus_regular, weight = FontWeight.Normal),
+    Font(R.font.eudoxus_bold, weight = FontWeight.Bold),
+    Font(R.font.eudoxus_semibold, weight = FontWeight.SemiBold),
 )
+@Immutable
+data class CustomTypography(
+    val title: Title,
+    val body: Body,
+    val caption: Caption
+) {
+    data class Title(
+        val hero: TextStyle,
+        val extraLarge: TextStyle,
+        val large: TextStyle,
+        val moderate: Moderate,
+        val small: Small,
+        val tiny: Tiny,
+    )
 
-@ExperimentalTextApi
-internal val GmsFontProvider: GoogleFont.Provider by lazy {
-    GoogleFont.Provider(
-        providerAuthority = "com.google.android.gms.fonts",
-        providerPackage = "com.google.android.gms",
-        certificates = R.array.com_google_android_gms_fonts_certs,
+    data class Body(
+        val moderate: Moderate,
+        val small: Small,
+    )
+
+    data class Caption(
+        val moderate: Moderate,
+        val small: Small
+    )
+
+    data class Moderate(
+        val regular: TextStyle,
+    ) {
+        val demi: TextStyle get() = regular.copy(fontWeight = FontWeight.SemiBold)
+        val book: TextStyle get() = regular.copy(fontWeight = FontWeight.Normal)
+    }
+
+    data class Small(
+        val regular: TextStyle,
+    ) {
+        val demi: TextStyle get() = regular.copy(fontWeight = FontWeight.SemiBold)
+        val bold: TextStyle get() = regular.copy(fontWeight = FontWeight.Bold)
+    }
+
+    data class Tiny(
+        val regular: TextStyle,
+    ) {
+        val demi: TextStyle get() = regular.copy(fontWeight = FontWeight.SemiBold)
+        val bold: TextStyle get() = regular.copy(fontWeight = FontWeight.Bold)
+    }
+}
+
+val LocalCustomTypography = staticCompositionLocalOf {
+    CustomTypography(
+        body = CustomTypography.Body(
+            moderate = CustomTypography.Moderate(
+                TextStyle.Default
+            ),
+            small = CustomTypography.Small(
+                TextStyle.Default
+            ),
+        ),
+        caption = CustomTypography.Caption(
+            moderate = CustomTypography.Moderate(
+                TextStyle.Default
+            ),
+            small = CustomTypography.Small(
+                TextStyle.Default
+            ),
+        ),
+        title = CustomTypography.Title(
+            hero = TextStyle.Default,
+            extraLarge = TextStyle.Default,
+            large = TextStyle.Default,
+            moderate = CustomTypography.Moderate(
+                TextStyle.Default
+            ),
+            small = CustomTypography.Small(
+                TextStyle.Default
+            ),
+            tiny = CustomTypography.Tiny(
+                TextStyle.Default
+            ),
+        )
     )
 }
 
-@OptIn(ExperimentalTextApi::class)
-val FMTypography = Typography(
-    defaultFontFamily = createSingleGoogleFontFamily(
-        name = "Poppins",
-        weights = listOf(
-            FontWeight.Light,
-            FontWeight.Normal,
-            FontWeight.Medium
+val foodMarketTypography = CustomTypography(
+    title = CustomTypography.Title(
+        hero = TextStyle(
+            fontFamily = fonts,
+            fontWeight = FontWeight.Bold,
+            fontSize = 28.sp,
+            lineHeight = 44.sp
+        ),
+        extraLarge = TextStyle(
+            fontFamily = fonts,
+            fontWeight = FontWeight.Bold,
+            fontSize = 24.sp,
+            lineHeight = 36.sp
+        ),
+        large = TextStyle(
+            fontFamily = fonts,
+            fontWeight = FontWeight.Bold,
+            fontSize = 21.sp,
+            lineHeight = 28.sp
+        ),
+        moderate = CustomTypography.Moderate(
+            regular = TextStyle(
+                fontFamily = fonts,
+                fontSize = 18.sp,
+                lineHeight = 24.sp
+            ),
+        ),
+        small = CustomTypography.Small(
+            regular = TextStyle(
+                fontFamily = fonts,
+                fontSize = 16.sp,
+                lineHeight = 20.sp
+            ),
+        ),
+        tiny = CustomTypography.Tiny(
+            regular = TextStyle(
+                fontFamily = fonts,
+                fontSize = 14.sp,
+                lineHeight = 20.sp
+            ),
         ),
     ),
+    body = CustomTypography.Body(
+        moderate = CustomTypography.Moderate(
+            regular = TextStyle(
+                fontFamily = fonts,
+                fontSize = 16.sp,
+                lineHeight = 20.sp,
+                fontWeight = FontWeight.Normal
+            ),
+        ),
+        small = CustomTypography.Small(
+            regular = TextStyle(
+                fontFamily = fonts,
+                fontSize = 14.sp,
+                lineHeight = 20.sp,
+                fontWeight = FontWeight.Normal
+            ),
+        ),
+    ),
+    caption = CustomTypography.Caption(
+        moderate = CustomTypography.Moderate(
+            regular = TextStyle(
+                fontFamily = fonts,
+                fontSize = 13.sp,
+                lineHeight = 16.sp,
+            ),
+        ),
+        small = CustomTypography.Small(
+            regular = TextStyle(
+                fontFamily = fonts,
+                fontSize = 12.sp,
+                lineHeight = 16.sp,
+                fontWeight = FontWeight.Normal
+            ),
+        ),
+    )
 )
